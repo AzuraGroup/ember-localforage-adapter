b540111bdd09e7966900005a7a47cb46fa0c19d5
PeterMead <petermead@petermead.info>
2016-10-25 16:27:47 +0100

    Always continue processing the queue when promises are settled.  Previously the queue stopped when a promise was rejected.

--- addon/utils/queue.js
+++ addon/utils/queue.js
@@ -8,7 +8,7 @@ export default Ember.Object.extend({
     const queueKey = this.queue.length;
 
     this.queue[queueKey] = new Ember.RSVP.Promise((resolve, reject) => {
-      this.queue[queueKey - 1].then(() => {
+      this.queue[queueKey - 1].finally(() => {
         this.queue.splice(queueKey - 1, 1);
         callback(resolve, reject);
       });
