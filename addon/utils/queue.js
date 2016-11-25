10c33c00cea14b4fb6594574f4f3d1d1aab01305
PeterMead <petermead@petermead.info>
2016-11-25 11:02:21 +0000

    Bug fix.  We can't use splice to change the size of the array because this queue depends on the indecies staying the same.

--- addon/utils/queue.js
+++ addon/utils/queue.js
@@ -9,7 +9,6 @@ export default Ember.Object.extend({
 
     this.queue[queueKey] = new Ember.RSVP.Promise((resolve, reject) => {
       this.queue[queueKey - 1].then(() => {
-        this.queue.splice(queueKey - 1, 1);
         callback(resolve, reject);
       });
     });
